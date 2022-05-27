f791899780e9df349c565afb801c0044a4bba9fb
Xizheng (Hugh) Yu <yuxizheng@outlook.com>
2022-05-27 17:37:47 -0500

    226 & 617

---
+++ data-structures/tree/226.java
@@ -0,0 +1,27 @@
+// 226. Invert Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        TreeNode right = invertTree(root.left);
+        TreeNode left = invertTree(root.right);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+}
