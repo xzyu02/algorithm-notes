621c05813eed10d2e39d93d55d2e262b8c8b06a8
Xizheng (Hugh) Yu <yuxizheng@outlook.com>
2022-06-02 23:55:10 -0500

    new questions added to repository

---
+++ others/others/304.java
@@ -0,0 +1,18 @@
+// 304. Range Sum Query 2D - Immutable
+
+class NumMatrix {
+    public int[][] dp;
+
+    public NumMatrix(int[][] matrix) {
+        dp = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i-1][j-1];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2+1][col2+1] + dp[row1][col1] - dp[row1][col2+1] - dp[row2+1][col1];
+    }
+}
